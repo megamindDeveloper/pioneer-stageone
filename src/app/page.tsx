bd3b5992e2aef9121fa356ad50bbbf93aa904d0d
Jamshadj <jamshadalij2004@gmail.com>
2025-08-11 21:14:10 +0530

    updated the models into images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,46 @@
 "use client";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DashcamShowcase from "../components/MainPageComponents/DashcamShowcase/DashcamShowcase";
 import ProductDetials from "../components/MainPageComponents/ProductDetailPage/ProductDetials";
 import ProductComparisonTable from "@/components/MainPageComponents/ProductComparisonTable/ProductComparisonTable";
 import FeatureAccordion from "@/components/MainPageComponents/FeatureAccordion/FeatureAccordion";
 import Footer from "@/components/CommonComponents/Footer";
 import FadeLoader from "@/components/CommonComponents/Loader";
+import DashcamShowcaseMobile from "@/components/MainPageComponents/DashcamShowcase/DashcamShowcaseMobile";
 const CameraScene = dynamic(() => import("../components/ThreeJsComponents/CameraScene"), {
   ssr: false,
 });
 
 export default function Home() {
   const [modelReady, setModelReady] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1024); // lg: <1024px
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <>
       {/* ✅ Loader rendered from the page itself */}
-      {/* {!modelReady && (
+      {!modelReady && (
         <div className="fixed inset-0 z-[9999] bg-black flex items-center justify-center">
           <FadeLoader isModelReady={false} />
         </div>
-      )} */}
+      )}
       <div className="w-full h-full bg-gradient-to-b ">
-        {/* <CameraScene onModelReady={() => setModelReady(true)} /> */}
-        <DashcamShowcase />
+        <CameraScene onModelReady={() => setModelReady(true)} />
+        {isMobile ? <DashcamShowcaseMobile /> : <DashcamShowcase />}
         <ProductDetials />
         <ProductComparisonTable />
-        <FeatureAccordion  />
+        <FeatureAccordion />
         <Footer />
         {/* <CameraScenetest/> */}
       </div>
     </>
   );
 }
-                                      
--- src/components/MainPageComponents/DashcamShowcase/DashcamShowcase.tsx
+++ src/components/MainPageComponents/DashcamShowcase/DashcamShowcase.tsx
@@ -155,243 +155,156 @@ export default function DashcamShowcase() {
 
         {/* Cards Grid */}
         <div className="relative z-20 grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-0 w-full h-full">
-          {isMobile ? (
-            <>
-              {" "}
-              <DashcamCardMobile
-                cardIndex={0}
-                title="VREC-Z820DC"
-                image="/modelImages/VREC-Z820DC/main.png"
-                imageClassName="!w-[100%] !h-60"
-                description={`4K Flagship Dual Channel
-      Dash Cam with Night Vision AI`}
-                features={[
-                  "4K Resolution\nFront Camera",
-                  "Full HD\nRear Camera",
-                  "Intelligent\nNight Vision AI",
-                  "WDR & HDR\nRecording",
-                  "ADAS\nAlerts",
-                ]}
-               
-                featureIcons={[
-                  "/homePageImages/dashboardShowcaseIcons/4kicon.svg",
-                  "/homePageImages/dashboardShowcaseIcons/fullHd.svg",
-                  "/homePageImages/dashboardShowcaseIcons/aivision.svg",
-                  "/homePageImages/dashboardShowcaseIcons/wdrhdr.svg",
-                  "/homePageImages/dashboardShowcaseIcons/adasAlert.svg",
-                ]}
-              />
-              <DashcamCardMobile
-                cardIndex={1}
-                title="VREC-H520DC"
-                 image="/modelImages/VREC-H520DC/main.png"
-                 imageClassName=" !w-[90%] mx-auto !h-56  mt-4"
-                description={`2K Dual Channel Dash Cam with
-      ADAS Alerts`}
-                features={["2K Quad HD\nFront Camera", "Full HD\nRear Camera", "Enhanced\nNight Vision", "HDR\nRecording", "ADAS\nAlerts"]}
-                featureIcons={[
-                  "/homePageImages/dashboardShowcaseIcons/2kicon.svg",
-                  "/homePageImages/dashboardShowcaseIcons/fullHd.svg",
-                  "/homePageImages/dashboardShowcaseIcons/enhanced.svg",
-                  "/homePageImages/dashboardShowcaseIcons/hdr.svg",
-                  "/homePageImages/dashboardShowcaseIcons/adasAlert.svg",
-                ]}
-              />
-              <DashcamCardMobile
-               
-                cardIndex={2}
-                title="VREC-H320SC"
-               
-                description={`Full HD Dash Cam
-      `}
-                features={["2 MP 1080P\nFront Camera", "WDR\nRecording", "In-built\nGPS Logger", "ADAS\nAlerts"]}
-                image="/modelImages/VREC-H320SC/main.png"
-                 imageClassName=" !w-[80%] mx-auto !h-52  mt-4"
-                featureIcons={[
-                  "/homePageImages/dashboardShowcaseIcons/2kicon.svg",
-                  "/homePageImages/dashboardShowcaseIcons/wdr.svg",
-                  "/homePageImages/dashboardShowcaseIcons/inBuildGps.svg",
-                  "/homePageImages/dashboardShowcaseIcons/adasAlert.svg",
-                ]}
-              />
-              <DashcamCardMobile
-             
-                cardIndex={3}
-                title="VREC-H120SC"
-             
-                description={`Ultra Compact 1.5K Dash Cam`}
-                features={["1.5K Resolution\nFront Camera", "Super\nCompact", "Emergency\nRecording", "Wide Field\nView", "In-built\nG-Sensor"]}
-                image="/modelImages/VREC-H120SC/main.png"
-                 imageClassName=" !w-[50%] mx-auto !h-52 mt-4"
-                featureIcons={[
-                  "/homePageImages/dashboardShowcaseIcons/1.5k.svg",
-                  "/homePageImages/dashboardShowcaseIcons/compact.svg",
-                  "/homePageImages/dashboardShowcaseIcons/emergency.svg",
-                  "/homePageImages/dashboardShowcaseIcons/wideView.svg",
-                  "/homePageImages/dashboardShowcaseIcons/inbuidSensor.svg",
-                ]}
-              />
-            </>
-          ) : (
-            <>
-              <DashcamCard
-                cardIndex={0}
-                title="VREC-Z820DC"
-                Component={<VRECZ820DC />}
-                directionalLights={[
-                  {
-                    position: [-0.24, 3.295, 10.316],
-                    hoveredPosition: [-22.58, 8.959, 15.017],
-                    intensity: 1,
-                    hoveredIntensity: 5,
-                    color: "#ffffff",
-                  },
-                  {
-                    position: [2.07, 8.164, 16.117],
-                    hoveredPosition: [-1.916, 5.069, 20.8],
-                    intensity: 1.5,
-                    hoveredIntensity: 5,
-                    color: "#ffffff",
-                  },
-                ]}
-                cameraPosition={[0, 1, 5]}
-                description={`4K Flagship Dual Channel
+          <DashcamCard
+            cardIndex={0}
+            title="VREC-Z820DC"
+            Component={<VRECZ820DC />}
+            directionalLights={[
+              {
+                position: [-0.24, 3.295, 10.316],
+                hoveredPosition: [-22.58, 8.959, 15.017],
+                intensity: 1,
+                hoveredIntensity: 5,
+                color: "#ffffff",
+              },
+              {
+                position: [2.07, 8.164, 16.117],
+                hoveredPosition: [-1.916, 5.069, 20.8],
+                intensity: 1.5,
+                hoveredIntensity: 5,
+                color: "#ffffff",
+              },
+            ]}
+            cameraPosition={[0, 1, 5]}
+            description={`4K Flagship Dual Channel
               Dash Cam with Night Vision AI`}
-                features={[
-                  "4K Resolution\nFront Camera",
-                  "Full HD\nRear Camera",
-                  "Intelligent\nNight Vision AI",
-                  "WDR & HDR\nRecording",
-                  "ADAS\nAlerts",
-                ]}
-                defaultScale={scaleZ820DC}
-                hoveredScale={hoveredScaleZ820DC}
-                defaultPosition={[0, 0.9, 0]}
-                hoveredPosition={[0.9, 0.9, 0]}
-                defaultRotation={[-0.2, 0, 0]}
-                hoveredRotation={[-0.2, -0.95, 0]}
-                featureIcons={[
-                  "/homePageImages/dashboardShowcaseIcons/4kicon.svg",
-                  "/homePageImages/dashboardShowcaseIcons/fullHd.svg",
-                  "/homePageImages/dashboardShowcaseIcons/aivision.svg",
-                  "/homePageImages/dashboardShowcaseIcons/wdrhdr.svg",
-                  "/homePageImages/dashboardShowcaseIcons/adasAlert.svg",
-                ]}
-              />
-              <DashcamCard
-                directionalLights={[
-                  {
-                    position: [-5.477, 20.381, 18.049],
-                    hoveredPosition: [-23.118, 10.792, 4.712],
-                    intensity: 1.5,
-                    hoveredIntensity: 4,
-                    color: "#ffffff",
-                  },
-                  {
-                    position: [-2.758, 12.887, 34.355],
-                    hoveredPosition: [-16.192, 10.811, 33.857],
-                    intensity: 1.5,
-                    hoveredIntensity: 4,
-                    color: "#ffffff",
-                  },
-                ]}
-                cardIndex={1}
-                title="VREC-H520DC"
-                Component={<VRECH520DC />}
-                cameraPosition={[0, 1, 5]}
-                description={`2K Dual Channel Dash Cam with
+            features={["4K Resolution\nFront Camera", "Full HD\nRear Camera", "Intelligent\nNight Vision AI", "WDR & HDR\nRecording", "ADAS\nAlerts"]}
+            defaultScale={scaleZ820DC}
+            hoveredScale={hoveredScaleZ820DC}
+            defaultPosition={[0, 0.9, 0]}
+            hoveredPosition={[0.9, 0.9, 0]}
+            defaultRotation={[-0.2, 0, 0]}
+            hoveredRotation={[-0.2, -0.95, 0]}
+            featureIcons={[
+              "/homePageImages/dashboardShowcaseIcons/4kicon.svg",
+              "/homePageImages/dashboardShowcaseIcons/fullHd.svg",
+              "/homePageImages/dashboardShowcaseIcons/aivision.svg",
+              "/homePageImages/dashboardShowcaseIcons/wdrhdr.svg",
+              "/homePageImages/dashboardShowcaseIcons/adasAlert.svg",
+            ]}
+          />
+          <DashcamCard
+            directionalLights={[
+              {
+                position: [-5.477, 20.381, 18.049],
+                hoveredPosition: [-23.118, 10.792, 4.712],
+                intensity: 1.5,
+                hoveredIntensity: 4,
+                color: "#ffffff",
+              },
+              {
+                position: [-2.758, 12.887, 34.355],
+                hoveredPosition: [-16.192, 10.811, 33.857],
+                intensity: 1.5,
+                hoveredIntensity: 4,
+                color: "#ffffff",
+              },
+            ]}
+            cardIndex={1}
+            title="VREC-H520DC"
+            Component={<VRECH520DC />}
+            cameraPosition={[0, 1, 5]}
+            description={`2K Dual Channel Dash Cam with
               ADAS Alerts`}
-                features={["2K Quad HD\nFront Camera", "Full HD\nRear Camera", "Enhanced\nNight Vision", "HDR\nRecording", "ADAS\nAlerts"]}
-                defaultScale={scaleH520DC}
-                hoveredScale={hoveredScaleH520DC}
-                defaultPosition={positionH520DC}
-                hoveredPosition={hoveredPositionH520DC}
-                defaultRotation={[-0.2, 0, 0]}
-                hoveredRotation={[-0.2, -0.75, 0]}
-                featureIcons={[
-                  "/homePageImages/dashboardShowcaseIcons/2kicon.svg",
-                  "/homePageImages/dashboardShowcaseIcons/fullHd.svg",
-                  "/homePageImages/dashboardShowcaseIcons/enhanced.svg",
-                  "/homePageImages/dashboardShowcaseIcons/hdr.svg",
-                  "/homePageImages/dashboardShowcaseIcons/adasAlert.svg",
-                ]}
-              />
-              <DashcamCard
-                directionalLights={[
-                  {
-                    position: [5.477, 20.372, 18.051],
-                    hoveredPosition: [-4.537, 9.827, 17.011],
-                    intensity: 4,
-                    hoveredIntensity: 4,
-                    color: "#ffffff",
-                  },
-                  {
-                    position: [-8.356, 18.164, 33.03],
-                    hoveredPosition: [-12.588, 6.504, 5.122],
-                    intensity: 10.5,
-                    hoveredIntensity: 5,
-                    color: "#ffffff",
-                  },
-                ]}
-                cardIndex={2}
-                title="VREC-H320SC"
-                Component={<VRECH320SC />}
-                cameraPosition={[0, 1, 5]}
-                description={`Full HD Dash Cam
+            features={["2K Quad HD\nFront Camera", "Full HD\nRear Camera", "Enhanced\nNight Vision", "HDR\nRecording", "ADAS\nAlerts"]}
+            defaultScale={scaleH520DC}
+            hoveredScale={hoveredScaleH520DC}
+            defaultPosition={positionH520DC}
+            hoveredPosition={hoveredPositionH520DC}
+            defaultRotation={[-0.2, 0, 0]}
+            hoveredRotation={[-0.2, -0.75, 0]}
+            featureIcons={[
+              "/homePageImages/dashboardShowcaseIcons/2kicon.svg",
+              "/homePageImages/dashboardShowcaseIcons/fullHd.svg",
+              "/homePageImages/dashboardShowcaseIcons/enhanced.svg",
+              "/homePageImages/dashboardShowcaseIcons/hdr.svg",
+              "/homePageImages/dashboardShowcaseIcons/adasAlert.svg",
+            ]}
+          />
+          <DashcamCard
+            directionalLights={[
+              {
+                position: [5.477, 20.372, 18.051],
+                hoveredPosition: [-4.537, 9.827, 17.011],
+                intensity: 4,
+                hoveredIntensity: 4,
+                color: "#ffffff",
+              },
+              {
+                position: [-8.356, 18.164, 33.03],
+                hoveredPosition: [-12.588, 6.504, 5.122],
+                intensity: 10.5,
+                hoveredIntensity: 5,
+                color: "#ffffff",
+              },
+            ]}
+            cardIndex={2}
+            title="VREC-H320SC"
+            Component={<VRECH320SC />}
+            cameraPosition={[0, 1, 5]}
+            description={`Full HD Dash Cam
               `}
-                features={["2 MP 1080P\nFront Camera", "WDR\nRecording", "In-built\nGPS Logger", "ADAS\nAlerts"]}
-                defaultScale={scaleH320SC}
-                hoveredScale={scaleH320SC}
-                defaultPosition={positionH320SC}
-                hoveredPosition={hoveredPositionH320SC}
-                defaultRotation={[-0.2, 0, 0]}
-                hoveredRotation={[-0.2, -0.75, 0]}
-                featureIcons={[
-                  "/homePageImages/dashboardShowcaseIcons/2kicon.svg",
-                  "/homePageImages/dashboardShowcaseIcons/wdr.svg",
-                  "/homePageImages/dashboardShowcaseIcons/inBuildGps.svg",
-                  "/homePageImages/dashboardShowcaseIcons/adasAlert.svg",
-                ]}
-              />
-              <DashcamCard
-                directionalLights={[
-                  {
-                    position: [-1.241, 8.582, 17.441],
-                    hoveredPosition: [-1.673, 5.556, 25.09],
-                    intensity: 4,
-                    hoveredIntensity: 8,
-                    color: "#ffffff",
-                  },
-                  {
-                    position: [5.477, 20.372, 18.051],
-                    hoveredPosition: [-1.84, 17.216, 1.162],
-                    intensity: 10.5,
-                    hoveredIntensity: 6,
-                    color: "#ffffff",
-                  },
-                ]}
-                cardIndex={3}
-                title="VREC-H120SC"
-                Component={<VRECH120SC />}
-                cameraPosition={[0, 1, 5]}
-                description={`Ultra Compact 1.5K Dash Cam`}
-                features={["1.5K Resolution\nFront Camera", "Super\nCompact", "Emergency\nRecording", "Wide Field\nView", "In-built\nG-Sensor"]}
-                defaultScale={scaleH120SC}
-                hoveredScale={hoveredScaleH120SC}
-                defaultPosition={positionH120SC}
-                hoveredPosition={hoveredPositionH120SC}
-                defaultRotation={[-0.2, 0, 0]}
-                hoveredRotation={[-0.2, -0.75, 0]}
-                featureIcons={[
-                  "/homePageImages/dashboardShowcaseIcons/1.5k.svg",
-                  "/homePageImages/dashboardShowcaseIcons/compact.svg",
-                  "/homePageImages/dashboardShowcaseIcons/emergency.svg",
-                  "/homePageImages/dashboardShowcaseIcons/wideView.svg",
-                  "/homePageImages/dashboardShowcaseIcons/inbuidSensor.svg",
-                ]}
-              />
-            </>
-          )}
+            features={["2 MP 1080P\nFront Camera", "WDR\nRecording", "In-built\nGPS Logger", "ADAS\nAlerts"]}
+            defaultScale={scaleH320SC}
+            hoveredScale={scaleH320SC}
+            defaultPosition={positionH320SC}
+            hoveredPosition={hoveredPositionH320SC}
+            defaultRotation={[-0.2, 0, 0]}
+            hoveredRotation={[-0.2, -0.75, 0]}
+            featureIcons={[
+              "/homePageImages/dashboardShowcaseIcons/2kicon.svg",
+              "/homePageImages/dashboardShowcaseIcons/wdr.svg",
+              "/homePageImages/dashboardShowcaseIcons/inBuildGps.svg",
+              "/homePageImages/dashboardShowcaseIcons/adasAlert.svg",
+            ]}
+          />
+          <DashcamCard
+            directionalLights={[
+              {
+                position: [-1.241, 8.582, 17.441],
+                hoveredPosition: [-1.673, 5.556, 25.09],
+                intensity: 4,
+                hoveredIntensity: 8,
+                color: "#ffffff",
+              },
+              {
+                position: [5.477, 20.372, 18.051],
+                hoveredPosition: [-1.84, 17.216, 1.162],
+                intensity: 10.5,
+                hoveredIntensity: 6,
+                color: "#ffffff",
+              },
+            ]}
+            cardIndex={3}
+            title="VREC-H120SC"
+            Component={<VRECH120SC />}
+            cameraPosition={[0, 1, 5]}
+            description={`Ultra Compact 1.5K Dash Cam`}
+            features={["1.5K Resolution\nFront Camera", "Super\nCompact", "Emergency\nRecording", "Wide Field\nView", "In-built\nG-Sensor"]}
+            defaultScale={scaleH120SC}
+            hoveredScale={hoveredScaleH120SC}
+            defaultPosition={positionH120SC}
+            hoveredPosition={hoveredPositionH120SC}
+            defaultRotation={[-0.2, 0, 0]}
+            hoveredRotation={[-0.2, -0.75, 0]}
+            featureIcons={[
+              "/homePageImages/dashboardShowcaseIcons/1.5k.svg",
+              "/homePageImages/dashboardShowcaseIcons/compact.svg",
+              "/homePageImages/dashboardShowcaseIcons/emergency.svg",
+              "/homePageImages/dashboardShowcaseIcons/wideView.svg",
+              "/homePageImages/dashboardShowcaseIcons/inbuidSensor.svg",
+            ]}
+          />
         </div>
       </div>
     </section>
